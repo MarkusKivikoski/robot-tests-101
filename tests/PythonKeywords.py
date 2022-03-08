c85c3a8be725d7d4f46565fd18c77ec3ddd779c5
MarkusKivikoski <markus.kivikoski@gmail.com>
2022-03-08 16:01:43 +0200

    onboarding example

--- tests/PythonKeywords.py
+++ tests/PythonKeywords.py
@@ -1,2 +1,16 @@
+from random import randint
+
 def lib_keyword(string):
     print(string)
+
+def find_answer(array, keyword):
+    for thing in array[0]:
+        if thing.get("name") == keyword:
+            return thing.get("answer_count")
+    raise Exception(f"{keyword} not found")
+
+def return_array_length(array):
+    return len(array[0])
+
+def generate_thing():
+    return "macaroni_" + str(randint(1,1000000))
--- tests/TestSuite.robot
+++ tests/TestSuite.robot
@@ -3,14 +3,12 @@ Library     Browser
 Resource    keywords.resource
 Resource    variables.resource
 
-
-
-
 *** Test Cases ***
-Test Case 1
-    Say Hello World !
 
-Example Test
-    New Page    http://espoonkopsu.fi:8022/
+Test Teaching
+    New Page    http://espoonkopsu.fi:8022/teach
     Get Title   ==  Things
-
+    Fill Thing Input
+    Answer Questions
+    Submit Answers
+    Check Answer
